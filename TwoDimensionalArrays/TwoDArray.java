b4444f7b61146f767e062370c1d6dce23a0a806e
Luke Hindman <lukehindman@boisestate.edu>
2020-10-26 18:17:30 -0700

    Lecture Video Wrap

--- TwoDimensionalArrays/TwoDArray.java
+++ TwoDimensionalArrays/TwoDArray.java
@@ -17,15 +17,18 @@ public class TwoDArray {
 		int[][] table = new int[5][10];
 
 		// Load the table with values
-		for (int row = 0; row < table.length; row++)
-			for (int col = 0; col < table[row].length; col++)
+		for (int row = 0; row < table.length; row++) {
+			for (int col = 0; col < table[row].length; col++) {
 				table[row][col] = row * table[row].length + col;
+			}
+		}
 
 		// Print the table
 		for (int row = 0; row < table.length; row++)
 		{
-			for (int col = 0; col < table[row].length; col++)
+			for (int col = 0; col < table[row].length; col++) {
 				System.out.print(table[row][col] + "\t");
+			}
 			System.out.println();
 		}
 	}
